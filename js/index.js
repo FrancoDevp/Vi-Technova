59ba4917809b2c5588a56093045657ca734560c4
FrancoDevp - React <franco.cantonati.99@gmail.com>
2022-09-22 14:46:38 -0300

    agregamos formulario con api wsp y modificamos imagenes

--- js/index.js
+++ js/index.js
@@ -1,10 +1,9 @@
 const evento = document.getElementById('send')
 const enviarFormulario =() => {
         let nombre = document.getElementById('nombres').value;
-        let producto = document.getElementById('producto').value;
         let mensaje = document.getElementById('mensaje').value;
         let numero= 543513760384;
 var win= window.open(`https://wa.me/${numero}?text=Hola%20mi%20nombre%20es%20${nombre}
-%20${producto}, Asunto:%20${mensaje}`,'_blank');       
+%20, Asunto:%20${mensaje}`,'_blank');       
 }
 evento.addEventListener('click', enviarFormulario)
